e75c575e4d8f7cbf143001c0905cf5d519af07ff
Zach Dahl <z.schtauffen@gmail.com>
2022-06-13 22:01:51 -0500

    comment out everything

--- crates/bevy_internal/src/prelude.rs
+++ crates/bevy_internal/src/prelude.rs
@@ -1,50 +1,54 @@
-#[doc(hidden)]
-#[cfg(not(target = "wasm32-wasi"))]
-pub use crate::{
-    app::prelude::*, asset::prelude::*, core::prelude::*, ecs::prelude::*, hierarchy::prelude::*,
-    input::prelude::*, log::prelude::*, math::prelude::*, reflect::prelude::*, scene::prelude::*,
-    transform::prelude::*, utils::prelude::*, window::prelude::*, DefaultPlugins, MinimalPlugins,
-};
+pub fn foo() {
+    println!("FOO");
+}
 
-#[cfg(not(target = "wasm32-wasi"))]
-pub use bevy_derive::{bevy_main, Deref, DerefMut};
+// #[doc(hidden)]
+// #[cfg(not(target = "wasm32-wasi"))]
+// pub use crate::{
+//     app::prelude::*, asset::prelude::*, core::prelude::*, ecs::prelude::*, hierarchy::prelude::*,
+//     input::prelude::*, log::prelude::*, math::prelude::*, reflect::prelude::*, scene::prelude::*,
+//     transform::prelude::*, utils::prelude::*, window::prelude::*, DefaultPlugins, MinimalPlugins,
+// };
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_audio")]
-pub use crate::audio::prelude::*;
+// #[cfg(not(target = "wasm32-wasi"))]
+// pub use bevy_derive::{bevy_main, Deref, DerefMut};
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_animation")]
-pub use crate::animation::prelude::*;
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_audio")]
+// pub use crate::audio::prelude::*;
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_core_pipeline")]
-pub use crate::core_pipeline::prelude::*;
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_animation")]
+// pub use crate::animation::prelude::*;
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_pbr")]
-pub use crate::pbr::prelude::*;
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_core_pipeline")]
+// pub use crate::core_pipeline::prelude::*;
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_render")]
-pub use crate::render::prelude::*;
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_pbr")]
+// pub use crate::pbr::prelude::*;
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_sprite")]
-pub use crate::sprite::prelude::*;
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_render")]
+// pub use crate::render::prelude::*;
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_text")]
-pub use crate::text::prelude::*;
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_sprite")]
+// pub use crate::sprite::prelude::*;
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_ui")]
-pub use crate::ui::prelude::*;
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_text")]
+// pub use crate::text::prelude::*;
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_dynamic_plugin")]
-pub use crate::dynamic_plugin::*;
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_ui")]
+// pub use crate::ui::prelude::*;
 
-#[doc(hidden)]
-#[cfg(feature = "bevy_gilrs")]
-pub use crate::gilrs::*;
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_dynamic_plugin")]
+// pub use crate::dynamic_plugin::*;
+
+// #[doc(hidden)]
+// #[cfg(feature = "bevy_gilrs")]
+// pub use crate::gilrs::*;
